2ec8ca4b06629b6f00521f41bf7e84edd8af6cc3
realouko <realouko@gmail.com>
2023-08-13 17:57:34 +0300

    ALX

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
-/*main function for printing*/
+/**
+ * main function for printing
+ */
 int main(void)
 {
-	/*main function with printf*/
+	/**
+	 * main function with printf
+	 */
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
