65c9ff7283d815e2b7edf1a89afb07720bc5f11f
realouko <realouko@gmail.com>
2023-08-14 19:43:08 +0300

    ALX

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,13 +7,13 @@
  */
 int main(void)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < 10; i++)
-    {
-        printf("%d\n", i);
-    }
+	for (i = 0; i < 10; i++)
+	{
+	printf("%d", i);
+	}
+	printf("\n");
 
-    return (0);
+	return (0);
 }
-
