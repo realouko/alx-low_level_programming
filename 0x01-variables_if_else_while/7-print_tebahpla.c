52874adfb0430064b53da4c5973b78978170de64
realouko <realouko@gmail.com>
2023-08-14 20:08:51 +0300

    ALX

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**main - entry
+ * Return zero
+ */
+int main(void)
+{
+	char letter;
+	for (letter = 'z'; letter >= 'a'; letter--)
+	{
+	putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
