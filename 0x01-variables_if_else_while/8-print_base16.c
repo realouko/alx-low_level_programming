36d0938ec7cb04fa81883bb8c07afa3e934acbbc
realouko <realouko@gmail.com>
2023-08-14 21:42:25 +0300

    ALX

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,21 +7,19 @@
  */
 int main(void)
 {
-    int i;
+	int i;
+	for (i = 0; i < 16; i++)
+	{
+	if (i < 10)
+	{
+	putchar(i + '0');
+	}
+	else
+	{
+	putchar(i - 10 + 'a');
+	}
+	}
 
-    for (i = 0; i < 16; i++)
-    {
-        if (i < 10)
-        {
-            putchar(i + '0');
-        }
-        else
-        {
-            putchar(i - 10 + 'a');
-        }
-    }
-
-    putchar('\n');
-
-    return (0);
+	putchar('\n');
+	return (0);
 }
