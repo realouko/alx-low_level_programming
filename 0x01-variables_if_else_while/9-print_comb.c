b0967b6796ed88a98f3292d70b1a607fe37f2f02
realouko <realouko@gmail.com>
2023-08-14 21:37:16 +0300

    ALX

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,20 +7,19 @@
  */
 int main(void)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < 10; i++)
-    {
-        putchar(i + '0');
+	for (i = 0; i < 10; i++)
+	{
+	putchar(i + '0');
 
-        if (i < 9)
-        {
-            putchar(',');
-            putchar(' ');
-        }
-    }
+	if (i < 9)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
 
-    putchar('\n');
-    return (0);
+	putchar('\n');
+	return (0);
 }
-
