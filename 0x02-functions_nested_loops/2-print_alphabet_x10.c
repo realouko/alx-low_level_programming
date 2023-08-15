ec340300d14d70e761fb09a5e8498100733c06b6
realouko <realouko@gmail.com>
2023-08-16 02:59:02 +0300

    ALX

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,7 +10,7 @@ void print_alphabet_x10(void)
 	char letter;
 	int i;
 
-	for (int i = 0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 	for (letter = 'a'; letter <= 'z'; letter++)
 	_putchar(letter);
