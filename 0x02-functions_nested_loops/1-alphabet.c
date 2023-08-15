6d42a6e199bf8469fdf36b42de228453fa62311a
realouko <realouko@gmail.com>
2023-08-16 01:33:28 +0300

    ALX

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* main - Entry point of the program
+*
+* Return: Always 0 (Success)
+*/
+void print_alphabet(void)
+{
+	char letter;
+
+	{
+	for (letter = 'a'; letter <= 'z'; letter++)
+	_putchar(letter);
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
