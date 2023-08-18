6ed203fe917e51413fd98e4cb29d18aef92f057d
realouko <realouko@gmail.com>
2023-08-18 11:51:21 +0300

    ALX

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* print_to_98: prints all natural numbers from n to 98
+* print_to_98 - this function prints all natural numbers from n to 98
 * followed by a newline
 * @n: reads integer i from stdin
 */
