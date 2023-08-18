f34a12387b6dafe33959976182467b88df4d40e0
realouko <realouko@gmail.com>
2023-08-18 13:08:35 +0300

    ALX

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+* main - prints the first 50 Fibonacci numbers, starting with 1 and 2, followed by a new line
+* Return: returns zero
+*/
+
+int main(void)
+{
+	long int a, b, c, nextfib;
+	b = 1;
+	c = 2;
+
+	for (a = 1; a <= 50; ++a)
+	{
+		if (b != 20365011074)
+		{
+			printf("%ld, ", b);
+		}
+		else
+		{
+			printf("%ld\n", b);
+		}
+
+		nextfib = b + c;
+		b = c;
+		c = nextfib;
+	}
+
+	return 0;
+}
+
