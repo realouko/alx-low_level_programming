fcfe5b896342850a8b6a90b08af5d34b62ec9656
realouko <realouko@gmail.com>
2023-08-18 14:26:35 +0300

    ALX

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* mul - entry point
+* @a: first operand
+* @b: second operand
+* Return: multiple of the two operands
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
