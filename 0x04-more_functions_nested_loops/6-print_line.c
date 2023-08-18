467d6edc24b2d0219983eff6e7fa2d3173a812b6
realouko <realouko@gmail.com>
2023-08-18 15:04:22 +0300

    ALX

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* print_line - to use _ character to print a line
+* n mumber of times
+* If n is 0, only newline will be printed
+*
+* @n: is the number of times the _ character
+* will be printed on the line
+*/
+
+void print_line(int n)
+{
+	int i;
+
+	for (i > 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
+
