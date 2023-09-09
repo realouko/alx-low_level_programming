61b1f952b8464a5ae38add0e43b57d251d9b7dc3
realouko <realouko@gmail.com>
2023-09-09 20:55:19 +0300

    ALX

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,6 +2,7 @@
 #define main_h
 
 int main(int argc __attribute__((unused)), char *argv[]);
+int main(int argc, char *argv[]);
 int main(int argc, char *argv[] __attribute__((unused)));
 int main(int argc, char *argv[]);
 
