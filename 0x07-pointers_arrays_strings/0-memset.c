c256e197222bccf59f97a74373526ad44d8f0fed
realouko <realouko@gmail.com>
2023-09-09 20:42:11 +0300

    ALX

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,16 +8,12 @@
 * Return: to return no value
 */
 
-
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; n > 0; i++, n--)
-	{
-		s[i] = b;
-	}
+	for (i = 0; i < n; i++)
+		*(s + i) =  b;
 
-	return (0);
+	return (s);
 }
-
