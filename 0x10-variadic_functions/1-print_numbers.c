f444dde802c374ecde667eae90cac02d69804fa7
realouko <realouko@gmail.com>
2023-09-15 21:09:12 +0300

    ALX

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,19 +12,18 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list valist;
 	unsigned int i;
+	va_list numbers;
 
-	va_start(valist, n);
+	va_start(numbers, n);
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(valist, int));
-		if (separator && i < n - 1)
+		printf("%d", va_arg(numbers, int));
+		if (i < n - 1 && separator != NULL)
 			printf("%s", separator);
 	}
-
 	printf("\n");
-	va_end(valist);
-}
 
+	va_end(numbers);
+}
