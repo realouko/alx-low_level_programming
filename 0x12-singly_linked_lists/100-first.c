f50277496383173c34d80f1398f108379b594ffb
realouko <realouko@gmail.com>
2023-09-22 19:00:57 +0300

    ALX

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include "lists.h"
 
-void before_main() __attribute__((constructor));
-
 /**
 *before_main -function that prints a string
 *before the main function is executed
@@ -16,3 +14,15 @@ void before_main()
 	printf("\nI bore my house upon my back!\n");
 }
 
+/**
+* main - prints a string on stdout after
+* the first function
+* return - to return void
+*
+*/
+
+int main(void)
+{
+	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+	return (0);
+}
