442fd52a86aa52c11ff1708c501e52967500fbc8
realouko <realouko@gmail.com>
2023-09-22 17:58:16 +0300

    ALX

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+* list_len - function that returns then number
+* of elements in a list_t list
+* @h: is a singly linked list
+* Return: to return the number
+* of elements in the list
+*/
+
+size_t list_len(const list_t *h)
+
+{
+	size_t nelem;
+
+	nelem = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		nelem++;
+	}
+
+	return (nelem);
+}
+
