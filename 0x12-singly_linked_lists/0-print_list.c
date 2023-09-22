687f664b532829e3db4d46fae62b67be0fbfd54a
realouko <realouko@gmail.com>
2023-09-22 17:17:03 +0300

    ALX

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+/**
+*print_list - function to print all
+*elements of the list_t list
+*@h: pointer to a list
+*
+*Return: to return the total
+*number of nodes.
+*/
+
+size_t print_list(const list_t *h)
+{
+	int count;
+	count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			h = h->next;
+			count++;
+		}
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
+
