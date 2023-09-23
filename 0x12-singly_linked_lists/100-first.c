8eabd558daaedb1f1f3e242cbbaf8bef47644e9c
realouko <realouko@gmail.com>
2023-09-23 03:39:10 +0300

    ALX

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,8 +3,8 @@
 
 void before_main(void) __attribute__((constructor));
 /**
-*before_main -function that prints a string
-*before the main function is executed
+* before_main -function that prints a string
+* before the main function is executed
 *
 * return - to return void
 */
@@ -15,15 +15,3 @@ void before_main(void)
 	printf("\nI bore my house upon my back!\n");
 }
 
-/**
-* main - prints a string on stdout after
-* the first function
-* return - to return void
-*
-*/
-
-int main(void)
-{
-	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
-	return (0);
-}
