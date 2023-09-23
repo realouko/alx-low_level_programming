2117b4135fa708e8228e929f4cf7537837fa0134
realouko <realouko@gmail.com>
2023-09-23 22:10:28 +0300

    ALX

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+* _atoi - function for converting a string to an integer
+* @s: pointer to string input
+* Return: integer output from conversion
+*/
+
+int _atoi(char *s)
+{
+	int i = 0;
+	unsigned int res = 0;
+	int signal = 1;
+
+	for (i = 0; s[i] != '\0'; ++i)
+	{
+		if (i[s] == '-')
+		{
+			signal *= -1;
+		}
+		if (i[s] >= '0' && i[s] <= '9')
+		{
+			res = res + s[i] - '0';
+			if ((*(s + (i + 1)) >= '0') && (*(s + (i + 1)) <= '9'))
+				res *= 10;
+			else
+				break;
+		}
+	}
+	return (res * signal);
+}
+
