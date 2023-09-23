b54829db95a69cbfe0e1473ceb0c42947c717d04
realouko <realouko@gmail.com>
2023-09-23 03:37:39 +0300

    ALX

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,7 +9,7 @@ void before_main(void) __attribute__((constructor));
 * return - to return void
 */
 
-void before_main()
+void before_main(void)
 {
 	printf("You're beat! and yet, you must allow,");
 	printf("\nI bore my house upon my back!\n");
