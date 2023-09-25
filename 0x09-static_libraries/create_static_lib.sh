86cddf435bb830b7fdd5cde92a8793331e616faa
realouko <realouko@gmail.com>
2023-09-25 21:09:23 +0300

    ALX

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-
-CC="gcc"
-CFLAGS="-Wall -Werror -Wextra -pedantic -std=gnu89"
-
-$CC $CFLAGS -c *.c
-
-ar rcs liball.a *.o
-
-rm -f *.o
-
-echo "Static library liball.a has been created."
+gcc -Wall -pedantic -Werror -Wextra -c *.c
+ar -rc liball.a *.o
+ranlib liball.a
