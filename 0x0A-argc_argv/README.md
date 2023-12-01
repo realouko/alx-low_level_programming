03e356cd2e43683ff48a27e4d03ee2b30e32ca88
realouko <133538980+realouko@users.noreply.github.com>
2023-12-01 22:22:47 +0300

    Delete 0x0A-argc_argv directory

--- 0x0A-argc_argv/README.md
+++
@@ -1,2 +0,0 @@
-This is a readme file fot the repository called
-alx-low_level_programming
--- 0x0A-argc_argv/main.h
+++
@@ -1,8 +0,0 @@
-#ifndef main_h
-#define main_h
-
-int main(int argc __attribute__((unused)), char *argv[]);
-int main(int argc, char *argv[] __attribute__((unused)));
-int main(int argc, char *argv[]);
-
-#endif
