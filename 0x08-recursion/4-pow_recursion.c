2200ed886d3d6420bbb6acd64b5b000e3faad72c
realouko <realouko@gmail.com>
2024-01-20 00:05:18 +0300

    RECURSION

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* _pow_recursion - Returns the value of x raised to y.
+* @x: base
+* @y: The exponent
+* Return: Pointer to the byte in `s` that matches one of the bytes in `accept`
+*/
+
+int _pow_recursion(int x, int y)
+{
+
+	if (y < 0)
+		return (-1);
+	else if (y == 1)
+		return (x);
+	else if (y == 0)
+		return (1);
+
+	return (x * _pow_recursion(x, y - 1));
+
+}
+
